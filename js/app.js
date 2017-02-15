b6971c1175a431e6e3176a3a62982cf03f77e4b5
arrianne <arrianneoshea@hotmail.co.uk>
2017-02-15 11:56:28 +0000

    bomb has now been added to minus points if hit

--- js/app.js
+++ js/app.js
@@ -4,13 +4,17 @@ $(() => {
   const $button = $('.startButton');
   const $difficultyButton = $('.difficultyButton');
   const $cells = $('.cell');
-  let speed = 1000;
+  let speed = 2000;
   let score = 0;
   const $easy = $('#easy');
   const $medium = $('#medium');
   const $hard = $('#hard');
   const $riddled = $('#riddled');
-  // const $riddler = $('riddler');
+  var $riddlerSound = $('#riddlerSound');
+  // var $riddleMe = $('#riddleMe');
+  let bombOrMole = null;
+  let bombOrMoleTimer = null;
+
 
 
 
@@ -34,53 +38,60 @@ $(() => {
         clearTimeout(timeOut);
       }
 
-    }, 100);
+    }, 113);
+
   }());
+
+
+
 //End of intro text
 
 //setting the speed for each difficulty level
 
   $easy.on('click', () => {
     speed = 2000;
-    // $riddler[0].play();
+    $riddlerSound[0].play();
     $button.text('Start Game');
 
   });
   $medium.on('click', () => {
     speed = 900;
+    $riddlerSound[0].play();
     $button.text('Start Game');
   });
   $hard.on('click', () => {
     speed = 400;
+    $riddlerSound[0].play();
     $button.text('Start Game');
   });
   $riddled.on('click', () => {
     speed = Math.floor(Math.random() * (2000 - 400)) + 400;
+    $riddlerSound[0].play();
     $button.text('Start Game');
   });
 
 //slowing down href
 
 
-  $(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
+  // $(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
 
-      var target = this.hash;
-      var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-      }, 1500, 'swing', function () {
-        window.location.hash = target;
-      });
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 1500, 'swing', function () {
+      window.location.hash = target;
     });
   });
+  // });
 
 
 //timer
 
-  var timer = 5;
+  var timer = 30;
 
 
 
@@ -88,60 +99,40 @@ $(() => {
 
   //button only to be pressed once until the time runs out
 
-
-
   function startTimer() {
     //resetting the score to zero
     score = 0;
-    $('#scoredisplay').text(score);
+    $('#scoredisplay').text('score: ' + score);
     //resetting the timer to 30
-    timer = 5;
-    $('#scoredisplay').text(score);
-
-
-
-
-
+    timer = 30;
     const timerId = setInterval(() => {
-      RandomMoleGenerator();
+      RandomGenerator();
       //removing the mole class when clicked
       $('.cell').click(function(e) {
         const hit = $(e.target);
-        if (!hit.hasClass('mole')){
-          return;
+        console.log(e.target);
+        if (hit.hasClass('mole')){
+          // return;
+
+          //Adding 1 to the players score everytime the mole class is clicked
+          score++;
+          console.log(score);
+          $('#scoredisplay').text('score: ' + score);
+        } else if (hit.hasClass('bomb')){
+          // return;
+          score--;
+          console.log(score);
+          $('#scoredisplay').text(score);
         }
-//Adding 1 to the players score everytime the mole class is clicked
-        score++;
-        console.log(score);
-        $('#scoredisplay').text(score);
 
-//possibly adding another thing for player to click that will take away points
-
-  //       RandomBombGenerator();
-  //       //removing the mole class when clicked
-  //       $('.cell').click(function(e) {
-  //         const hit = $(e.target);
-  //         if (!hit.hasClass('bomb')){
-  //           return;
-  //         }
-  // //Adding 1 to the players score everytime the mole class is clicked
-  //         score--;
-  //         console.log(score);
-  //         $('#scoredisplay').text(score);
-
-
-
-//bringing up the bat image when you hit mole
-        function showBat() {
+        if(hit.hasClass('mole') || hit.hasClass('bomb')) {
+          clearTimeout(bombOrMoleTimer);
+          hit.removeClass(bombOrMole);
+          hit.addClass('bat');
           setTimeout(function(){
             hit.removeClass('bat');
           }, 300);
         }
-
-        hit.removeClass('mole').addClass('bat');
-        showBat();
-
-
 
       });
       timer--;
@@ -155,6 +146,7 @@ $(() => {
         $button.text('Replay');
         $button.show();
         $difficultyButton.show();
+
       }
     }, speed);
 
@@ -163,31 +155,32 @@ $(() => {
     $button.hide();
     setTimeout(()=>{
       $button.show();
-    },timer*5000);
+    },timer*30000);
   }
 
 
 //picks random cells to change
 
-  function RandomMoleGenerator () {
-    const mole = Math.floor(Math.random()*$('.cell').length);
-    $cells.eq(mole).addClass('mole');
-    setTimeout(function() {
-      $cells.eq(mole).removeClass('mole');
+  function RandomGenerator () {
+    //this chooses random cell to generate mole or bomb
+
+    const cell = Math.floor(Math.random()*$('.cell').length);
+
+    //this chooses to show a bomb 20% of the time and mole(riddler) 80% of the time
+    bombOrMole = Math.random() < 0.2 ? 'bomb' : 'mole';
+    console.log(bombOrMole);
+
+    $cells.eq(cell).addClass(bombOrMole);
+    bombOrMoleTimer = setTimeout(() => {
+      $cells.eq(cell).removeClass(bombOrMole);
     },speed);
+
   }
 
 
-  // function RandomBombGenerator () {
-  //   const mole = Math.floor(Math.random()*$('.cell').length);
-  //   $cells.eq(bomb).addClass('bomb');
-  //   setTimeout(function() {
-  //     $cells.eq(bomb).removeClass('bomb');
-  //   },speed);
-  // }
-  //
 
 });
+
 
 
 //create end game function which fills ul with bat logo and score?
